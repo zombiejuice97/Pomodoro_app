db7ac738c5b7eb2894e90965c92a045ba0f1e022
azan <azan16shaikh@gmail.com>
2023-02-24 02:58:53 +0530

    add alarm sound

--- frontend/src/components/Tasks.js
+++ frontend/src/components/Tasks.js
@@ -1,7 +1,7 @@
 
 import React from "react";
 
-import 'reactjs-popup/dist/index.css';
+// import 'reactjs-popup/dist/index.css';
 
 
 function Tasks({ todo, deleteTodo, bringToForm, timerHandler }) {
@@ -10,9 +10,8 @@ function Tasks({ todo, deleteTodo, bringToForm, timerHandler }) {
             <div class="card" styles="width: 18rem;">
                 <div class="card-body">
                     <h5 class="card-title">{todo.name}  </h5>
-                    <p class="card-text">Completed? {todo.completed}</p>
-                    <a href="#" class="card-link">Start timer</a>
-                    <a href="#" class="card-link">Deadline</a>
+                    <p class="card-text">Completed? {todo.completed? "Yes": "No" }</p>
+                   
                     <br />
                     <button onClick={() => deleteTodo(todo._id)}>Delete</button>
                     <button onClick={() => bringToForm(todo)}>Edit</button>
--- frontend/src/components/Timer/Timer.js
+++ frontend/src/components/Timer/Timer.js
@@ -1,115 +1,104 @@
-import React, { useState,useEffect,useRef } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
 import "../Timer/timer.css"
 import ReactSlider from "react-slider";
+import alarm from "../Timer/alarm/alarm.wav"
 
-function Timer({ timerHandler,name }) {
-  const [isPaused,setIsPaused] = useState(true)
-  
+function Timer({ timerHandler, name }) {
+  const [isPaused, setIsPaused] = useState(true)
+
   const red = "#ea0823";
-  const green = "#15f207"
+  const green = "#05df12"
 
-  const [workMinutes,setWorkMinutes] = useState(30)
-  const [breakMinutes,setBreakMinutes] = useState(10)
-  const[secondsLeft,setSecondsLeft] = useState(0)
-  const [mode,setMode]=useState("work") //work,break,null
+  const [workMinutes, setWorkMinutes] = useState(30)
+  const [breakMinutes, setBreakMinutes] = useState(10)
+  const [secondsLeft, setSecondsLeft] = useState(0)
+  const [mode, setMode] = useState("work") //work,break,null
+  
 
-  const secondsLeftRef =useRef(secondsLeft);
+  const secondsLeftRef = useRef(secondsLeft);
   const isPausedRef = useRef(isPaused);
   const modeRef = useRef(mode);
 
 
-  function initTimer(){
-    setSecondsLeft(workMinutes*60);
-  }
-
-  function tick(){
+  function tick() {
     secondsLeftRef.current--;
     setSecondsLeft(secondsLeftRef.current);
-    
+
   }
 
- function switchMode(){
-  const nextMode = modeRef.current === 'work'? 'break' : 'work';
-  const nextSeconds = (nextMode === 'work'? workMinutes : breakMinutes) * 60;
   
-  setMode(nextMode);
-  modeRef.current = nextMode;
-  setSecondsLeft(nextSeconds);
-  secondsLeftRef.current = nextSeconds;
- }
 
-  function setAlarm(){
-    return 
+  function ring() {
+    new Audio(alarm).play();
   }
 
-  useEffect(()=>{
-    initTimer()
-    
-    const interval = setInterval(()=>{
-      if (isPausedRef.current){
+  useEffect(() => {
+
+    function switchMode() {
+      const nextMode = modeRef.current === 'work' ? 'break' : 'work';
+      const nextSeconds = (nextMode === 'work' ? workMinutes : breakMinutes) * 60;
+  
+      setMode(nextMode);
+      modeRef.current = nextMode;
+      setSecondsLeft(nextSeconds);
+      secondsLeftRef.current = nextSeconds;
+    }
+
+    secondsLeftRef.current = workMinutes * 60;
+    setSecondsLeft(secondsLeftRef.current);
+
+    const interval = setInterval(() => {
+      if (isPausedRef.current) {
         return
       }
-      if(secondsLeftRef.current==0){
-        // setIsPaused(true);
-        // setAlarm();
+      if (secondsLeftRef.current == 0) {
+        isPausedRef.current = true;
+        ring();
         switchMode();
       }
 
       tick();
-    },1000)
+    }, 1000)
 
-    return ()=>clearInterval(interval);
-  },[workMinutes,breakMinutes,isPaused,secondsLeft])
+    return () => clearInterval(interval);
+  }, [workMinutes, breakMinutes, setWorkMinutes,setBreakMinutes])
 
   const totalSeconds = mode === 'work' ? workMinutes * 60 : breakMinutes * 60;
-  const percentage = Math.round(secondsLeft / totalSeconds * 100); 
+  const percentage = Math.round(secondsLeft / totalSeconds * 100);
 
   const minutes = Math.floor(secondsLeft / 60);
   let seconds = secondsLeft % 60;
-  if(seconds < 10) seconds = '0'+seconds;
+  if (seconds < 10) seconds = '0' + seconds;
 
   return (
     <main>
       <div className="position-absolute top-50 start-50 translate-middle" style={{ width: 450, height: 300 }}>
-        <h1>{name}</h1>
+        <h1 style={{color:"white"}}>{name}</h1>
         <br />
         <CircularProgressbar value={percentage} text={minutes + ':' + seconds}
           styles={buildStyles({
-            // Rotation of path and trail, in number of turns (0-1)
             rotation: 0.25,
-
-            // Whether to use rounded or flat corners on the ends - can use 'butt' or 'round'
             strokeLinecap: 'butt',
-
-            // Text size
             textSize: '16px',
-
-            // How long animation takes to go from one percentage to another, in seconds
             pathTransitionDuration: 0.5,
-
-            // Can specify path transition in more detail, or remove it entirely
-            // pathTransition: 'none',
-
-            // Colors
-            pathColor: mode === "work"? red : green,
+            pathColor: mode === "work" ? red : green,
             textColor: '#f88',
             trailColor: '#d6d6d6',
             backgroundColor: '#3e98c7',
           })} />
-          
+
         <div className="buttons">
-          <div>
-            {isPaused?<button onClick={()=>{setIsPaused(false); isPausedRef.current = false}}>Play</button>
-            :
-            <button onClick={()=>{setIsPaused(true); isPausedRef.current = true}}>Pause</button>}
           
+            {isPaused ? <button onClick={() => { setIsPaused(false); isPausedRef.current = false }}>Play</button>
+              :
+              <button onClick={() => { setIsPaused(true); isPausedRef.current = true }}>Pause</button>}
           
-          </div>
-          
-          <button onClick={() => timerHandler(name)}>Back</button>
+
         </div>
+
+        <button className="back" onClick={() => timerHandler(name)}>Back</button>
 
         <div className="settings">
           <label>Work minutes:{workMinutes}</label>
@@ -117,7 +106,7 @@ function Timer({ timerHandler,name }) {
             className={'slider'}
             thumbClassName={'thumb'}
             trackClassName={'track'}
-            onChange={newValue=> setWorkMinutes(newValue)}
+            onChange={newValue => setWorkMinutes(newValue)}
             value={workMinutes}
             min={10}
             max={60}
@@ -128,7 +117,7 @@ function Timer({ timerHandler,name }) {
             className={'slider green'}
             thumbClassName={'thumb green'}
             trackClassName={'track'}
-            onChange={newValue=>setBreakMinutes(newValue)}
+            onChange={newValue => setBreakMinutes(newValue)}
             value={breakMinutes}
             min={5}
             max={30}
